d021ce12d3c17de5882efdd4d5870fe0ebfa6c0d
Jorgiy <jorgiy92@gmail.com>
2020-02-05 01:18:50 +0100

    add bubble sort

---
+++ Algorithms.Core/BubbleSort.cs
@@ -0,0 +1,28 @@
+﻿namespace Algorithms.Core
+{
+    using System.Collections.Generic;
+
+    public static class BubbleSort
+    {
+        public static void Sort<T>(IList<T> collectionToSort, IComparer<T> comparer = null)
+        {
+            if (comparer == null)
+            {
+                comparer = Comparer<T>.Default;
+            }
+
+            for (var i = 0; i <= collectionToSort.Count - 2; i++)
+            {
+                for (var j = 0; j <= collectionToSort.Count - 2; j++)
+                {
+                    if (comparer.Compare(collectionToSort[j], collectionToSort[j + 1]) > 0)
+                    {
+                        var temporaryElement = collectionToSort[j + 1];
+                        collectionToSort[j + 1] = collectionToSort[j];
+                        collectionToSort[j] = temporaryElement;
+                    }
+                }
+            }
+        }
+    }
+}
